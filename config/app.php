5948dfd78cdfbdc59921b23d4fb0f558174dca75
mohamed179 <mohamed77zaki@gmail.com>
2018-11-14 12:31:47 +0200

    Installing yajra/laravel-datatables package

--- config/app.php
+++ config/app.php
@@ -173,6 +173,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // yajra/laravel-datatables
+        Yajra\DataTables\DataTablesServiceProvider::class,
+
     ],
 
     /*
